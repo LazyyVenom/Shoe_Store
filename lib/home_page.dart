3c25ec88e0a1c78c580d3b90ac6e317826aa75a0
Anubhav <choubey.anubhav253@gmail.com>
2024-08-06 17:51:37 +0530

    Added Chips

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,9 +1,21 @@
 import "package:flutter/material.dart";
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
   final List<String> filters = const ['All', 'Addidas', 'Nike', 'Bata'];
+  late String selectedFilter;
+
+  @override
+  void initState() {
+    super.initState();
+    selectedFilter = filters[0];
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -13,33 +25,62 @@ class HomePage extends StatelessWidget {
           padding: const EdgeInsets.all(8.0),
           child: Column(
             children: [
-              Row(
+              const Row(
                 children: [
-                  const Text(
+                  Text(
                     'Shoe\nCollection',
                     style: TextStyle(
                       fontSize: 35,
                     ),
                   ),
-                  const Expanded(
+                  Expanded(
                     child: TextField(
                       decoration: InputDecoration(
                         hintText: "Search",
                         prefixIcon: Icon(Icons.search),
                         border: OutlineInputBorder(
                           borderRadius: BorderRadius.horizontal(
-                              left: Radius.circular(50),
-                              ),
+                            left: Radius.circular(50),
+                          ),
                         ),
                         focusedBorder: OutlineInputBorder(
                           borderRadius: BorderRadius.horizontal(
-                              left: Radius.circular(50),
-                              ),
+                            left: Radius.circular(50),
+                          ),
                         ),
                       ),
                     ),
                   ),
                 ],
+              ),
+              SizedBox(
+                height: 120,
+                child: ListView.builder(
+                    itemCount: filters.length,
+                    scrollDirection: Axis.horizontal,
+                    itemBuilder: (context, index) {
+                      final String filter = filters[index];
+                      return Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 8),
+                        child: GestureDetector(
+                          onTap: () {
+                            setState(() {
+                              selectedFilter = filter;
+                            });
+                          },
+                          child: Chip(
+                            padding: const EdgeInsets.all(10),
+                            label: Text(filter),
+                            shape: const RoundedRectangleBorder(
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(50))),
+                            backgroundColor: selectedFilter == filter
+                                ? Colors.amber
+                                : Colors.white,
+                          ),
+                        ),
+                      );
+                    }),
               ),
             ],
           ),
