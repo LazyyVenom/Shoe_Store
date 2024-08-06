b39e1e0f122647d906feeb80c52019d327c8dfb1
Anubhav <choubey.anubhav253@gmail.com>
2024-08-06 20:29:16 +0530

    Updated global vars

--- lib/global_vars.dart
+++ lib/global_vars.dart
@@ -1,5 +1,34 @@
 final products = [
   {
-    
-  }
+    'id': 0,
+    'title': 'Nike Shoes',
+    'company': 'Nike',
+    'price': 45.82,
+    'sizes': [8,9,10,11],
+    'imageUrl': 'assets/images/shoes_1.png',
+   },
+  {
+    'id': 1,
+    'title': 'Addidas Shoes',
+    'company': 'Addidas',
+    'price': 42.82,
+    'sizes': [7,8,9,10,11],
+    'imageUrl': 'assets/images/shoes_2.png',
+   },
+  {
+    'id': 2,
+    'title': 'Bata Shoes',
+    'company': 'Bata',
+    'price': 48.82,
+    'sizes': [8,9,10,11,12],
+    'imageUrl': 'assets/images/shoes_3.png',
+   },
+  {
+    'id': 3,
+    'title': 'Nike Shoes 2',
+    'company': 'Nike',
+    'price': 47.42,
+    'sizes': [7,8,9,10,11],
+    'imageUrl': 'assets/images/shoes_4.png',
+   }
 ];
